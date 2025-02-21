4a57fb221cf3a1f1408b04e7d30a18abee7f67a1
GooDGooN <qwe91911@gmail.com>
2025-02-21 16:18:18 +0900

    임시저장

---
+++ Assets/Scripts/FacebookLogin.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using Facebook.Unity;
+using NUnit.Framework;
+using System.Collections.Generic;
+using TMPro;
+public class FacebookLogin : MonoBehaviour
+{
+    public TMP_Text StatusText;
+    private void Awake()
+    {
+        if(FB.IsInitialized)
+        {
+            FB.ActivateApp();
+            SendStatusMessage("ActiveApp");
+        }
+        else
+        {
+            FB.Init(InitCallback, OnHideUnity);
+            void InitCallback()
+            {
+                if (FB.IsInitialized)
+                {
+                    FB.ActivateApp();
+                    SendStatusMessage("Facebook SDK initialize successed");
+                }
+                else
+                {
+                    SendStatusMessage("Facebook SDK initialize failed");
+                }
+            }
+
+            void OnHideUnity(bool isResumeable)
+            {
+                if (isResumeable)
+                {
+                    Time.timeScale = 1.0f;
+                }
+                else
+                {
+                    Time.timeScale = 0.0f;
+                }
+            }
+        }   
+    }
+
+    public void LoginButtonClick()
+    {
+        List<string> permissions = new () { "public_profiles", "email" };
+        SendStatusMessage("button clicked");
+        FB.LogInWithReadPermissions(permissions, (ILoginResult result) =>
+        {
+            if (FB.IsLoggedIn)
+            {
+                var accessToken = AccessToken.CurrentAccessToken;
+                SendStatusMessage($"user id : {accessToken.UserId}");
+            }
+            else
+            {
+                SendStatusMessage("Login canceled");
+            }
+        });
+
+    }
+
+    public void SendStatusMessage(string message)
+    {
+        StatusText.text = message;
+    }
+}
---
+++ Assets/Scripts/MessageLogs.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MessageLogs : MonoBehaviour
+{
+    public TMP_Text ErrorText;
+    private Queue<string> logMessages = new();
+    private void Start()
+    {
+        Application.logMessageReceived += LogHandler;
+    }
+
+    private void LogHandler(string logString, string stackTrace, LogType logType)
+    {
+        logMessages.Enqueue("[" + logType.ToString().ToUpper() + "] " + logString);
+        while (logMessages.Count > 3)
+        {
+            logMessages.Dequeue();
+        }
+        ErrorText.text = "";
+        foreach (var message in logMessages)
+        {
+            ErrorText.text += message;
+            ErrorText.text += "\n";
+        }
+    }
+}
