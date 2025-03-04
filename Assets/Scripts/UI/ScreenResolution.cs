d221cf260c26d1bbdc32560238b195eb8f99c72f
GooDGooN <qwe91911@gmail.com>
2025-03-04 14:11:16 +0900

    해상도 설정, 가로 화면으로 고정되게 설정,

---
+++ Assets/Scripts/UI/ScreenResolution.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ScreenResolution : MonoBehaviour
+{
+    private int targetWidth = 640;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+    }
+    private void OnEnable()
+    {
+        var screenWidth = Screen.width;
+        var screenHeight = Screen.height;
+        var ratio = (float)screenHeight / screenWidth;
+        Screen.SetResolution(targetWidth, (int)(ratio * targetWidth), true);
+    }
+
+}
